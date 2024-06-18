f5b971a0bd41e087cd6f2bce5600b8855c691738
ilmenichetti <ilmenichetti@gmail.com>
2024-06-18 11:53:15 +0300

    Updated with more commands (vignette also updated)

--- R/API_wrappers.R
+++ R/API_wrappers.R
@@ -55,14 +55,12 @@ get_token <- function(uname, upass){
 
 #' Get projects
 #'
-#' This function is utilized to get the projects assigned to a certain user
+#' This function is utilized to get the projects assigned to the user
 #'
 #' @maintainer Lorenzo Menichetti, \email{lorenzo.menichetti@@luke.fi}
 #' @author Jani Anttila, \email{jani.anttila@@luke.fi}
-#' @param uname username
-#' @param upass password
-#' @param toke output straight from the function \code{\link{get_token_LM}}.
-#' @return Description of the return value.
+#' @param token output straight from the function \code{\link{get_token_LM}}.
+#' @return a data.frame (which should have 9 variables:  "id", "worker", "worker_name", "project", "project_name", "author", "author_name", "date", "active"). This might change in different versions of the database, eventually contact the package maintainer.
 #' @details Any additional details about the function.
 #' @examples
 #' get_projects(mytoken)
@@ -78,6 +76,141 @@ get_projects <- function(token){
 }
 
 
+#' Get the list of measurement sets
+#'
+#' This function is utilized to get the list of measurement sets in a given project
+#'
+#' @maintainer Lorenzo Menichetti, \email{lorenzo.menichetti@@luke.fi}
+#' @author Jani Anttila, \email{jani.anttila@@luke.fi}
+#' @param token output straight from the function \code{\link{get_token_LM}}.
+#' @param project_id id of the project the user wants to query
+#' @return a data frame of 17 variables, and as many rows as the measurements events
+#' @details each row corresponds to a measurement set, with the relative metadata
+#' @examples
+#' all_meas <- get_meas(mytoken, project_id = 1)
+#' @export
+get_meas <- function(token, project_id){
+  access_list = create_access_list()
+  req_url <- paste(access_list$protocol ,"://",access_list$ip ,access_list$meas_addr ,project_id ,sep="")
+  req <- request(req_url) |> req_headers(Authorization = paste("Token",token))
+  resp <- req_perform(req)
+  resp_json <- resp |> resp_body_json()
+  meas <- do.call(rbind.data.frame, resp_json$measurements)
+  return(meas)
+}
 
 
 
+#' Get the readings for one series of measurements
+#'
+#' This function is utilized to gccess one specific series of measurements
+#'
+#' @maintainer Lorenzo Menichetti, \email{lorenzo.menichetti@@luke.fi}
+#' @author Jani Anttila, \email{jani.anttila@@luke.fi}
+#' @param token output straight from the function \code{\link{get_token_LM}}.
+#' @param meas_id password
+#' @return a list of lists
+#' @details Things here gets more complicated
+#' @examples
+#' all_meas <- get_meas(mytoken, project_id = 1)
+#' @export
+get_series <- function(token,meas_id){
+  access_list = create_access_list()
+  req_url <- paste(access_list$protocol,"://",access_list$ip,access_list$series_addr,meas_id,sep="")
+  req <- request(req_url) |> req_headers(Authorization = paste("Token",token))
+  resp <- req_perform(req)
+  resp_json <- resp |> resp_body_json()
+  return(resp_json$series)
+}
+
+
+
+
+#' Get all the subsite id in a series
+#'
+#' This function is utilized to extract all the subsite ID in a series
+#'
+#' @maintainer Lorenzo Menichetti, \email{lorenzo.menichetti@@luke.fi}
+#' @author Lorenzo Menichetti, \email{lorenzo.menichetti@@luke.fi}
+#' @param series object from the function \code{\link{get_series}}.
+#' @return a data frame
+#' @details Things here gets more complicated
+#' @examples
+#' subsite_info <- subsiteID_names(series)
+#' @export
+# Function to extract subsiteID and names
+subsiteID_names <- function(series) {
+  subsite_info <- do.call(rbind, lapply(series, function(x) {
+    data.frame(subsiteid = x$subsiteid, siteid = x$siteid, stringsAsFactors = FALSE)
+  }))
+  return(subsite_info)
+}
+
+
+#' Get all the point id in a series
+#'
+#' This function is utilized to extract all the points (collars) ID in a series
+#'
+#' @maintainer Lorenzo Menichetti, \email{lorenzo.menichetti@@luke.fi}
+#' @author Jani Anttila, \email{jani.anttila@@luke.fi}
+#' @param series object from the function \code{\link{get_series}}.
+#' @return a data frame
+#' @details Things here gets more complicated
+#' @examples
+#' points_info <- point_names(series)
+#' @export
+# Function to extract subsiteID and names
+point_names <- function(series) {
+  subsite_info <- do.call(rbind, lapply(series, function(x) {
+    data.frame(subsiteid = x$subsiteid, siteid = x$siteid, point = x$point, stringsAsFactors = FALSE)
+  }))
+  return(subsite_info)
+}
+
+
+#' Filter all the elements in a series based on a subsite id
+#'
+#' This function is utilized to extract all the elements (measurements time series) in a series that correspond to a specific subsite ID
+#'
+#' @maintainer Lorenzo Menichetti, \email{lorenzo.menichetti@@luke.fi}
+#' @author Lorenzo Menichetti, \email{lorenzo.menichetti@@luke.fi}
+#' @param series object from the function \code{\link{get_series}}.
+#' @return a list of lists as the function \code{\link{get_token_LM}}, but subsetted for a single subsite ID.
+#' @details Things here gets more complicated
+#' @examples
+#' subsiteID_filter(series, subsiteID_value="DP-MC")
+#' @export
+# Function to filter based on subsiteID
+subsiteID_filter <- function(series, subsiteID_value) {
+  filtered_list <- lapply(series, function(x) {
+    if (x$subsiteid == subsiteID_value) {
+      return(x)
+    }
+  })
+
+  # Remove NULL elements from the filtered list
+  filtered_list <- filtered_list[!sapply(filtered_list, is.null)]
+  return(filtered_list)
+}
+
+
+
+#' Reformat one single element of a series in a more readable format
+#'
+#' This function is utilized to extract all the elements (measurements time series) in a series that correspond to a specific subsite ID
+#'
+#' @maintainer Lorenzo Menichetti, \email{lorenzo.menichetti@@luke.fi}
+#' @author Lorenzo Menichetti, \email{lorenzo.menichetti@@luke.fi}
+#' @param series object from the function \code{\link{get_series}}.
+#' @return a list for one single element of the series
+#' @details Things here gets more complicated
+#' @examples
+#' process_single_series(series_list=series, 11)
+#' @export
+# Function to filter based on subsiteID
+process_single_series <- function(series ,index){
+  part <- series[[index]]
+  values <- unlist(part$values)
+  part$values <- values
+  return(part)
+}
